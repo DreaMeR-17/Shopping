a2a6053ca244f07ab8c2d576f90fd0c0ef71224b
DreaMeR-17 <pronka_1993@mail.ru>
2024-07-09 12:43:06 +0600

    Update Program.cs
    more space

--- Shopping/Program.cs
+++ Shopping/Program.cs
@@ -2,15 +2,16 @@
 
 namespace Shopping
 {
+    
     internal class Program
     {
+        
         static void Main(string[] args)
         {
             
-            int costForCrystal = 12;
+            int crystalPrice = 12;
             int goldOfPlayer;
             int purchasedCrystals;
-
 
             Console.WriteLine("Приветствую вам в моем магазине Кристаллов!");
             Console.WriteLine("Один кристалл за 12 золотых. Хорошее предложение, верно?");
@@ -22,12 +23,14 @@ namespace Shopping
             Console.Write("Сколько хотите купить кристаллов? ");
             purchasedCrystals = Convert.ToInt32(Console.ReadLine());
 
-            goldOfPlayer -= (purchasedCrystals * costForCrystal);
+            goldOfPlayer -= (purchasedCrystals * crystalPrice);
 
             Console.Write("У вас осталось золота: " + goldOfPlayer) ;
             Console.WriteLine(" ");
             Console.Write("Вы купили " + purchasedCrystals + " кристаллов.");
 
         }
+        
     }
+    
 }
